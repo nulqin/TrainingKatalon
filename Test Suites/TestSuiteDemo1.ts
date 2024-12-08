19e298b4c0c809728644bc76b87e0a8f6bd60888
bogow <achmad.yaqinn@gmail.com>
2024-12-08 21:00:56 +0700

    Commit Update

--- Test Suites/TestSuiteDemo1.ts
+++ Test Suites/TestSuiteDemo1.ts
@@ -11,11 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>67bea153-13ac-4be9-93e1-4a033bec2afe</testSuiteGuid>
-   <testCaseLink>
-      <guid>7e474d3b-2df2-44ce-9f7d-4d8b00abd370</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Demo/ScriptDemo</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
